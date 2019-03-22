4ec7f934485b82a9ad0bb70561606273c711b30b
iaburton <iaburton@users.noreply.github.com>
2019-03-22 14:17:49 -0500

    Flush out Handler and Options

--- fakesentry.go
+++ fakesentry.go
@@ -3,8 +3,10 @@ package fakesentry
 import (
 	"bytes"
 	"compress/zlib"
+	"context"
 	"encoding/base64"
 	"encoding/json"
+	"errors"
 	"io"
 	"io/ioutil"
 	"log"
@@ -13,10 +15,33 @@ import (
 	"os"
 )
 
+var (
+	//ErrBadMethod is passed into an ErrorHandler if the client uses an unexpected HTTP method.
+	ErrBadMethod = errors.New("fakesentry: Bad HTTP method from client")
+
+	//ErrBadContentLength is passed into an ErrorHandler if the client sends an invalid content length.
+	ErrBadContentLength = errors.New("fakesentry: Bad ContentLength from client")
+
+	//ErrBadContentType is passed into an ErrorHandler if the clients sends an unexpected content type.
+	ErrBadContentType = errors.New("fakesentry: Bad ContentType from client")
+)
+
+//Handler implements a basic ServeHTTP method that can be used to accept and parse HTTP requests from
+//Sentry clients. The Handler type is meant for testing, usually guarded by a development or build flag.
+//It is generally unsafe to use in production unless guarded by other HTTP Handlers, such as something
+//that checks the requests auth state.
 type Handler struct {
 	logger Logger
+
+	eh   ErrorHandler
+	next http.Handler
 }
 
+//NewHandler returns a new Handler with defaults unless Options are set.
+//Unless otherwise changed, the default logger is a stdlib 'log' that prints to STDERR.
+//The default ErrorHandler prints the error to the log and writes a 400 or 500 code to the client.
+//If the Handler is not used in middleware then the default action is to dump the HTTP request
+//as well as pretty print the Sentry JSON into the logger.
 func NewHandler(opts ...Option) Handler {
 	h := new(Handler)
 	for _, opt := range opts {
@@ -31,33 +56,43 @@ func (h *Handler) withDefaults() {
 	if h.logger == nil {
 		h.logger = log.New(os.Stderr, "[FAKESENTRY] ", log.LstdFlags)
 	}
+
+	if h.eh == nil {
+		h.eh = h.errorHandler
+	}
 }
 
-func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	b, err := httputil.DumpRequest(r, false)
-	if err != nil {
-		h.logger.Printf("DumpRequest: %v", err)
+func (h Handler) errorHandler(w http.ResponseWriter, r *http.Request, err error) {
+	switch err {
+	case ErrBadContentLength, ErrBadContentType, ErrBadMethod:
+		h.logger.Printf("%v", err)
+		w.WriteHeader(http.StatusBadRequest)
+	default:
+		h.logger.Printf("Error from ServeHTTP: %v", err)
 		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
+
+//ServeHTTP implements http.Handler. Please refer to the Handler type definiton
+//NewHandler function for more information.
+func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		h.eh(w, r, ErrBadMethod)
 		return
 	}
 
-	if r.ContentLength <= 0 {
-		h.logger.Printf("Headers for BadRequest\n%s", b)
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	} else if r.ContentLength > (1024 * 1024 * 32) {
-		//Check for some absurd content length
-		//32mb should be plenty, right?
-		h.logger.Printf("Headers for BadRequest\n%s", b)
-		w.WriteHeader(http.StatusBadRequest)
+	//Check for some absurd content length
+	//32mb should be plenty, right?
+	if r.ContentLength <= 0 || r.ContentLength > (1024*1024*32) {
+		h.eh(w, r, ErrBadContentLength)
 		return
 	}
 
 	var jb []byte
 	if ct := r.Header.Get("Content-Type"); ct == "application/json" {
-		jb = make([]byte, r.ContentLength)
+		jb = make([]byte, r.ContentLength) //not something you want to do in production
 
-		_, err = io.ReadAtLeast(r.Body, jb, int(r.ContentLength))
+		_, err := io.ReadAtLeast(r.Body, jb, int(r.ContentLength))
 		if err != nil {
 			h.logger.Printf("ReadAtLeast: %v", err)
 			w.WriteHeader(http.StatusBadRequest)
@@ -82,8 +117,19 @@ func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 	} else {
-		h.logger.Printf("Headers for BadRequest\n%s", b)
-		w.WriteHeader(http.StatusBadRequest)
+		h.eh(w, r, ErrBadContentType)
+		return
+	}
+
+	if h.next != nil {
+		ctx := context.WithValue(r.Context(), rawMessage, json.RawMessage(jb))
+		h.next.ServeHTTP(w, r.WithContext(ctx))
+		return
+	}
+
+	b, err := httputil.DumpRequest(r, false)
+	if err != nil {
+		h.eh(w, r, err)
 		return
 	}
 
--- options.go
+++ options.go
@@ -1,15 +1,69 @@
 package fakesentry
 
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+)
+
+//Option is a functional option to help configure a Handler.
 type Option func(*Handler)
 
+//Logger is the basic logging interface used by this package.
 type Logger interface {
 	Printf(format string, arg ...interface{})
 }
 
+//WithLogger returns an Option that sets a Logger on a Handler.
 func WithLogger(logger Logger) Option {
 	return func(h *Handler) {
 		if logger != nil {
 			h.logger = logger
+		}
+	}
+}
+
+//ErrorHandler is called when an error occurs within Handler.ServeHTTP.
+//The error is guaranteed to be non-nil, the handler may use the passed http types
+//to communicate to the client what happened.
+type ErrorHandler func(http.ResponseWriter, *http.Request, error)
+
+//WithErrorHandler returns an Option for an ErrorHandler
+func WithErrorHandler(eh ErrorHandler) Option {
+	return func(h *Handler) {
+		if eh != nil {
+			h.eh = eh
+		}
+	}
+}
+
+type ctxKey int
+
+const (
+	rawMessage ctxKey = iota
+)
+
+//FromContext returns a json.RawMessage that was saved if AsMiddleware
+//is used with the Handler type. Look to FromRequest for more information.
+func FromContext(ctx context.Context) (jb json.RawMessage, ok bool) {
+	jb, ok = ctx.Value(rawMessage).(json.RawMessage)
+	return
+}
+
+//FromRequest retrieves the saved json.RawMessage Handler.ServeHTTP saves
+//into the request context if AsMiddleware is used. Other http.Handlers can
+//use this to retrieve the value and do with it as they will.
+func FromRequest(r *http.Request) (json.RawMessage, bool) {
+	return FromContext(r.Context())
+}
+
+//AsMiddleware takes the next http.Handler and changes the behavior of
+//Handler.ServeHTTP, in that if a http.Handler is set, the parsed Sentry JSON
+//value will be saved into the http.Requests context for use by the next http.Handler.
+func AsMiddleware(next http.Handler) Option {
+	return func(h *Handler) {
+		if next != nil {
+			h.next = next
 		}
 	}
 }
